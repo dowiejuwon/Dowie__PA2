5051e841989e2691acd574f5d7cb59edb8d2a067
Juwon Dowie <diggityd@juwons-air.lan>
2019-09-21 18:01:53 -0400

    final commit

--- AssignmentTwo/src/Problem3_Dowie.java
+++ AssignmentTwo/src/Problem3_Dowie.java
@@ -4,7 +4,7 @@ public class Problem3_Dowie {
 
 
 	   public static void main(String args[]){
-	       String[] topic={"Sports","Religion","Fashion","Music","Movies"}; 
+	       String[] cat={"Sports","Religion","Fashion","Music","Movies"}; 
 	       int[][] answers=new int[5][11];
 	       for(int i=0;i<4;i++){
 	           for(int j=1;j<=10;j++){
@@ -16,17 +16,17 @@ public class Problem3_Dowie {
 	       System.out.print("Please enter the amount of people that is going to rate: ");
 	       Scanner scnr = new Scanner(System.in);
 	       numPeople=scnr.nextInt();
-	       int i=1;
+	       int i = 1;
 	       while(i<=numPeople){
 	           for(int j=0;j<5;j++){ 
-	               System.out.print("Person"+i+" "+topic[j]+": ");
+	               System.out.print("Person "+i+" "+cat[j]+": ");
 	               int a = scnr.nextInt();
 	               answers[j][a]=answers[j][a]+1;
 	           }
 	           i++;
 	       }
-	       System.out.println("Topic "+"Total Rating "+"Avg Rating");
-	       int maxPeople=0,minPeople=100000,maxTopic=0,minTopic=0;
+	       System.out.println("Topic "+"   Total Rating "+"   Avg Rating");
+	       int maxPeople=0,minPeople=100000,maxCat=0,minCat=0;
 	       for(i=0;i<5;i++)
 	       {
 	           int total=0,count=0;
@@ -38,18 +38,18 @@ public class Problem3_Dowie {
 	           if(maxPeople<total)
 	           {  
 	               maxPeople=total;
-	               maxTopic=i;
+	               maxCat=i;
 	           }
 	           if(minPeople>total)
 	           {  
 	               minPeople=total;
-	               minTopic=i;
+	               minCat=i;
 	           }
-	           System.out.println(topic[i]+" "+total+" "+(total+0.0)/count);
+	           System.out.println(cat[i]+"      "+total+"                 "+(total+0.0)/count);
 	       }
-	       System.out.println("Max Point Topic "+"Total Rating");
-	       System.out.println(topic[maxTopic]+" "+maxPeople);
-	       System.out.println("Min Point Topic "+"Total Rating");
-	       System.out.println(topic[minTopic]+" "+minPeople);
+	       System.out.print("Max Point Topic "+"Total Rating: ");
+	       System.out.println(cat[maxCat]+" "+maxPeople);
+	       System.out.print("Min Point Topic "+"Total Rating: ");
+	       System.out.println(cat[minCat]+" "+minPeople);
 	   }
 	}
